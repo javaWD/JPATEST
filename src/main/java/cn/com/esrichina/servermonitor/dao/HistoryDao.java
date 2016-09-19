37a1fd2879509117029da135b5d887aa0ffc72ae
wangdong123 <drudgery@live.com>
2016-09-19 16:07:27 +0800

    add the query method of lastest time

--- src/main/java/cn/com/esrichina/servermonitor/dao/HistoryDao.java
+++ src/main/java/cn/com/esrichina/servermonitor/dao/HistoryDao.java
@@ -1,7 +1,12 @@
 package cn.com.esrichina.servermonitor.dao;
 
-import cn.com.esrichina.servermonitor.domain.HistoryId;
+import java.util.List;
+
+import cn.com.esrichina.servermonitor.domain.History;
+import cn.com.esrichina.servermonitor.util.QueryHistoryOption;
 
 public interface HistoryDao {
-	public void search(HistoryId hid);
+	public List<History>  search(QueryHistoryOption queryHistoryOption);
+	
+	public List<History>  searchLastestTime(QueryHistoryOption queryHistoryOption);
 }
--- src/main/java/cn/com/esrichina/servermonitor/dao/HistoryDaoImpl.java
+++ src/main/java/cn/com/esrichina/servermonitor/dao/HistoryDaoImpl.java
@@ -1,12 +1,17 @@
 package cn.com.esrichina.servermonitor.dao;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import cn.com.esrichina.servermonitor.domain.History;
 import cn.com.esrichina.servermonitor.domain.HistoryId;
+import cn.com.esrichina.servermonitor.util.QueryHistoryOption;
 
 @Repository("historyDao")
 public class HistoryDaoImpl implements HistoryDao{
@@ -16,9 +21,24 @@ public class HistoryDaoImpl implements HistoryDao{
 	
 
 	@Transactional
-	public void search(HistoryId hid) {
+	public List<History> search(QueryHistoryOption qHistoryOption) {
 		//em.find(HistoryId.class,hid);
-		em.createQuery("");
+		List<History> lhis=em.createNamedQuery("History.conditionQuery").
+				setParameter("id",new BigInteger(qHistoryOption.getItemId().toString())).
+				setParameter("queryStart",qHistoryOption.getQueryStart()).
+				setParameter("queryEnd", qHistoryOption.getQueryEnd()).
+				getResultList();
+		return lhis;
+	}
+
+	@Transactional
+	@Override
+	public List<History> searchLastestTime(QueryHistoryOption queryHistoryOption) {
+		List<History> lhis=em.createNamedQuery("History.findLastestTime").
+				setParameter("id",new BigInteger(queryHistoryOption.getItemId().toString())).
+				setMaxResults(1).
+				getResultList();
+		return lhis;
 	}
 
 }
