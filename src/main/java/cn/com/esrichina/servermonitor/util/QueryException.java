6f3aedfa4bc2428dfca7951a0656f4c95e0a209b
wangdong123 <drudgery@live.com>
2016-09-19 16:14:46 +0800

    add the serial version uid remove the useless properties

--- src/main/java/cn/com/esrichina/servermonitor/util/QueryException.java
+++ src/main/java/cn/com/esrichina/servermonitor/util/QueryException.java
@@ -1,8 +1,12 @@
 package cn.com.esrichina.servermonitor.util;
 
 public class QueryException extends Exception{
-	private QueryErrorType errorType;
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1315062046978891884L;
+
 	public QueryException(String message) {
 		super(message);
 	}
