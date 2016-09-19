dcff5ea9742c28c6b954bb852d5b65eaa05d0adb
wangdong123 <drudgery@live.com>
2016-09-19 15:54:13 +0800

    add the Constrants of test

---
+++ src/test/java/cn/com/esrichina/JPATEST/MonitorConstants.java
@@ -0,0 +1,10 @@
+package cn.com.esrichina.JPATEST;
+
+public class MonitorConstants {
+
+	public static final String ItemsId =  "23252";
+	public static final String HostId = "10108";
+	
+	public static final int Start = 1471364432;
+	public static final int End =1471370432;
+}
